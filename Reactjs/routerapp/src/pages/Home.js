b2c3c56912ed65459c9dfab4087095032b7f85d6
azartechnet <azartechnet@gmail.com>
2025-01-04 19:36:35 +0530

    updated

--- Reactjs/routerapp/src/pages/Home.js
+++ Reactjs/routerapp/src/pages/Home.js
@@ -1,4 +1,53 @@
-const Home=()=>{
-    return<h1>Home</h1>
-}
+import { Carousel } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import c1 from './images/c1.jpeg';
+
+import c2 from './images/c2.jpeg';
+
+import c3 from './images/c3.jpeg';
+
+import './Home.css';
+const Home = () => {
+    return (
+        <div>
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={c1}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>First Slide Label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={c2}
+                        alt="Second slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>Second Slide Label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={c3}
+                        alt="Third slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>Third Slide Label</h3>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+            <h1>Welcome</h1>
+        </div>
+    );
+};
+
 export default Home;
--- Reactjs/routerapp/src/pages/Layout.js
+++ Reactjs/routerapp/src/pages/Layout.js
@@ -1,16 +1,30 @@
 import { Outlet,Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 const Layout=()=>{
     return (
         <div>
-            <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/blogs">Blogs</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
-
-                </ul>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/">MyApp</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/blogs">Blogs</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/contact">Contact</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </nav>
-            <Outlet/>
+            <Outlet />
         </div>
     )
 }
