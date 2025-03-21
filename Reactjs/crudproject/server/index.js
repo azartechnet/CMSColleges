c56746da753d4420d21e2d80a0a65b810d188798
azartechnet <azartechnet@gmail.com>
2025-03-21 18:54:45 +0530

    Update index.js

--- Reactjs/crudproject/server/index.js
+++ Reactjs/crudproject/server/index.js
@@ -14,7 +14,9 @@ app.use(express.json())
 
 const FoodModel=require("./models/Food")
 
-mongoose.connect("mongodb://localhost:27017/food")
+mongoose.connect("mongodb+srv://admin:admin@cluster0.j8tqf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/food")
+
+// mongoose.connect("mongodb://localhost:27017/food")
 .then(() => console.log('Connected to MongoDB...'))
 .catch(err => console.error('Could not connect to MongoDB...')) 
 
@@ -73,7 +75,7 @@ app.put("/update",async(req,res)=>{
 app.delete("/delete/:id",async(req,res)=>{
   const id = req.params.id; 
   try{
-    const result=await FoodModel.findByIdAndRemove(id);
+    const result=await FoodModel.findByIdAndDelete(id);
     if(!result)
     {
        return res.status(404).send("Food items not found")
